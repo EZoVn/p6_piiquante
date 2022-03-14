5694541428ea8649d0845f5424e03da991c0fa3a
EzoVn <vincent.galpin@gmail.com>
2022-03-14 15:55:59 +0100

    ajout de passwordValidator

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const pwd = require('../middleware/password-validator');
 
-router.post('/signup', userCtrl.signup);
+router.post('/signup', pwd.passwordSchema, userCtrl.signup);
 router.post('/login', userCtrl.login);
 
 module.exports = router;
