4cc400fe2d1aca581b4bc8755d4399deb1fbb418
EzoVn <vincent.galpin@gmail.com>
2022-03-21 10:50:12 +0100

    modif gitignore

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -38,7 +38,7 @@ exports.login = (req, res, next) => {
             bcrypt.compare(req.body.password, user.password)
                 .then(valid => {
                     if (!valid) {
-                        return res.status(401).json({ error: 'Mot de passe incorect !' });
+                        return res.status(401).json({ error: 'message' });
                     }
                     res.status(200).json({
                         userId: user._id,
@@ -49,7 +49,7 @@ exports.login = (req, res, next) => {
                         )
                     });
                 })
-                .catch(error => res.status(500).json({ error }));
+                .catch(error => res.status(500).json({ message: "error" }));
         })
-        .catch(error => res.status(500).json({ error }));
+        .catch(error => res.status(500).json({message: "toto" }));
 };
